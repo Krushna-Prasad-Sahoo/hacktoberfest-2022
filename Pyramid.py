0f964112d96413ecc7e3ccd74a83fb60d90518e8
Miki s <68747673+MIHIRs01@users.noreply.github.com>
2022-10-03 14:27:16 +0530

    Created pyramid with word pyramid in python

---
+++ Pyramid.py
@@ -0,0 +1,11 @@
+## pyramid of word python
+
+s = "python"
+for i in range(0, len(s)):
+    for j in range(0, i + 1):
+        print(s[j], end='')
+    print('')
+for i in range(len(s), 0, -1):
+    for j in range(0, i - 1):
+        print(s[j], end='')
+    print('')
